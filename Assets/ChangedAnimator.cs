557f6e2b2a89b0aed6fe2371e857224b0d1ac34b
Jaruwat Taengnuan <darkness.state.001@gmail.com>
2022-02-25 20:27:07 +0700

    first commit

---
+++ Assets/ChangedAnimator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangedAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Changed animators
+    public void getAnimateDance(){
+        this.GetComponent<Animator>().Play("dance");
+    }
+    public void getAnimateWolking(){
+        this.GetComponent<Animator>().Play("walking");
+    }
+}
---
+++ Assets/ChangedColor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangedColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Function Changed Colors
+    public void getRed()
+    {
+        this.GetComponent<Renderer>().material.color = Color.red;
+    }
+    public void getGreen()
+    {
+        this.GetComponent<Renderer>().material.color = Color.green;
+    }
+    public void getBlue()
+    {
+        this.GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
